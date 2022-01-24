ffb59dce8b5b6977462f13bae3f234bd36dc4244
yurio <vika.tsyganova.dubna@gmail.com>
2022-01-25 01:40:53 +0300

    главный экран, ходы

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ matrix_1 = [[0] * 10 for i in range(10)]
 matrix_2 = [[0] * 10 for j in range(10)]
 RED_FLAG = False
 RED_FLAG_POLE = False
+UNCLICK = False
 P4_COUNT = 1
 P3_COUNT = 2
 P2_COUNT = 3
@@ -64,34 +65,39 @@ class Board():
             return x, y
         return None
 
-    def on_click(self, cell_coords, n, side, pole):
-        global P4_COUNT, P3_COUNT, P2_COUNT, P1_COUNT  # переменные показывающие количество возможных кораблей тех или иных палуб
+    def on_click(self, cell_coords, n, side, pole, pos):
+        global P4_COUNT, P3_COUNT, P2_COUNT, P1_COUNT, UNCLICK  # переменные показывающие количество возможных кораблей тех или иных палуб
         #  то есть изначально р4 = 1 а после уставноки на поле 4палубного корабля вычитаю один из переменной
+        UNCLICK = True
+        if side == 'right':
+            BOARD3.on_click('', 0, 0, 0, pos)
+        elif side == 'down':
+            BOARD4.on_click('', 0, 0, 0, pos)
         if n == 4:  # решила использовать n чтобы узнать колво палуб у нажатого корабля
             if P4_COUNT == 0:  # если переменная равна 0 то выводится сообещин на экран и кораблик не ставится
                 error_count_ships()
             else:
-                self.ok_click(cell_coords, n, side, pole)
+                self.ok_click(cell_coords, n, side, pole, pos)
         elif n == 3:
             if P3_COUNT == 0:
                 error_count_ships()
             else:
-                self.ok_click(cell_coords, n, side, pole)
+                self.ok_click(cell_coords, n, side, pole, pos)
 
         elif n == 2:
             if P2_COUNT == 0:
                 error_count_ships()
             else:
-                self.ok_click(cell_coords, n, side, pole)
+                self.ok_click(cell_coords, n, side, pole, pos)
 
         elif n == 1:
             if P1_COUNT == 0:
                 error_count_ships()
             else:
-                self.ok_click(cell_coords, n, side, pole)
+                self.ok_click(cell_coords, n, side, pole, pos)
 
-    def ok_click(self, cell_coords, n, side, pole):
-        global RED_FLAG, P4_COUNT, P3_COUNT, P2_COUNT, P1_COUNT
+    def ok_click(self, cell_coords, n, side, pole, pos):
+        global RED_FLAG, P4_COUNT, P3_COUNT, P2_COUNT, P1_COUNT, UNCLICK
         if pole == 1:
             self.matrix = matrix_1
         else:
@@ -107,6 +113,8 @@ class Board():
                         error_place()
 
                 if not RED_FLAG:
+                    UNCLICK = True
+                    BOARD3.on_click('', 0, 0, 0, pos)
                     if n == 4:
                         P4_COUNT -= 1
                     if n == 3:
@@ -151,6 +159,7 @@ class Board():
                 print(elem)
 
         elif side == 'down':
+            UNCLICK = True
             if cell_coords[1] + n <= 10:
                 for i in range(n):
                     if self.matrix[cell_coords[1] + i][cell_coords[0]] == 1 \
@@ -159,6 +168,8 @@ class Board():
                         error_place()
 
                 if not RED_FLAG:
+                    UNCLICK = True
+                    BOARD4.on_click('', 0, 0, 0, pos)
                     if n == 4:
                         P4_COUNT -= 1
                     if n == 3:
@@ -200,27 +211,23 @@ class Board():
             for elem in matrix_1:
                 print(elem)
 
-    def get_click(self, mouse_pos, n, side, pole):
+    def get_click(self, mouse_pos, n, side, pole, pos):
         cell = self.get_cell(mouse_pos)
         if cell:
             self.on_click(cell, n, side,
-                          pole)
+                          pole, pos)
 
 
 # класс для того чтобы в матрицу не заносились данные корабликов для выбора
 class Board_Choose_Ship(Board):
-    def on_click(self, cell_coords, n, side, board):
-        self.board[cell_coords[1]][cell_coords[0]] = 1 - self.board[cell_coords[1]][cell_coords[0]]
 
-
-BOARD1 = Board(10, 10)
-BOARD1.set_view(45, 85, 35)
-BOARD2 = Board(10, 10)
-BOARD2.set_view(440, 85, 35)
-BOARD3 = Board_Choose_Ship(4, 1)
-BOARD3.set_view(140, 495, 35)
-BOARD4 = Board_Choose_Ship(4, 1)
-BOARD4.set_view(460, 495, 35)
+    def on_click(self, cell_coords, n, side, board, pos):
+        global UNCLICK
+        if UNCLICK:
+            self.board[pos[1]][pos[0]] = 0
+            UNCLICK = False
+        else:
+            self.board[cell_coords[1]][cell_coords[0]] = 1 - self.board[cell_coords[1]][cell_coords[0]]
 
 
 def del_error_f():
@@ -242,7 +249,7 @@ def error_place():
 def error_count_ships():
     del_error_f()
     font = pygame.font.Font(None, 32)
-    text = font.render('Вы уже использовали все кораблики этого типа. Выберете другой', True, (255, 255, 255))
+    text = font.render('Вы уже использовали все кораблики этого типа. Выберите другой', True, (255, 255, 255))
     screen.blit(text, (40, 555))
 
 
@@ -270,17 +277,37 @@ def hide_pole(pole):
         BOARD2.set_view(440, 85, 35)
 
 
+def player(n):
+    pygame.draw.rect(screen, (0, 0, 0), (225, 500, 270, 100), 0)
+    font_names = pygame.font.SysFont('arial', 45)
+    if n % 2 == 0:
+        name = font_names.render("Player 1", True, (255, 255, 255))
+    else:
+        name = font_names.render("Player 2", True, (255, 255, 255))
+    screen.blit(name, (230, 500))
+
+
+BOARD1 = Board(10, 10)
+BOARD1.set_view(45, 85, 35)
+BOARD2 = Board(10, 10)
+BOARD2.set_view(440, 85, 35)
+BOARD3 = Board_Choose_Ship(4, 1)
+BOARD3.set_view(140, 495, 35)
+BOARD4 = Board_Choose_Ship(4, 1)
+BOARD4.set_view(460, 495, 35)
+
+
 def start_screen():
     screen.fill((0, 0, 0))
     font_1 = pygame.font.Font(None, 50)
     font_2 = pygame.font.Font(None, 32)
     rules = ['Морской бой',
-             'Игра рассчитана на двух человек, играющих на одном устройстве', 'Первым расставляет корабли PLAYER 1',
-             'Чтобы это сделать, нужно нажать на корабль,', 'а затем на клетку поля, откуда будет построен кораблик',
-             'Чтобы зафиксировать местоположение всех кораблей, нажмите "w"',
-             'Тогда корабли будет расставлять PLAYER 2', 'Первым ходит PLAYER 1',
-             'Чтобы сделать ход, нажмите на клетку, куда вы хотите выстрелить', 'После победы нажмите "пробел",',
-             'чтобы увидеть результаты игры', 'Нажмите "пробел", чтобы начать игру']
+             '- Игра рассчитана на двух человек, играющих на одном устройстве', '- Первым расставляет корабли PLAYER 1',
+             '- Чтобы это сделать, нужно нажать на корабль,', 'а затем на клетку поля, откуда будет построен кораблик',
+             '- Чтобы зафиксировать местоположение всех кораблей, нажмите "w"',
+             '- Тогда корабли будет расставлять PLAYER 2', '- Первым ходит PLAYER 1',
+             '- Чтобы сделать ход, нажмите на клетку, куда вы хотите выстрелить', '- После победы нажмите "пробел",',
+             'чтобы увидеть результаты игры', '- Нажмите "пробел", чтобы начать игру']
     y = 15
     for elem in rules:
         if elem == 'Морской бой':
@@ -368,6 +395,7 @@ def main_screen():
     ship4vert.rect.x = 567
     ship4vert.rect.y = 497
 
+    pos = ''
     n = 333
     side = ''
     kol = 1
@@ -377,33 +405,36 @@ def main_screen():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.MOUSEBUTTONDOWN and event.pos[1] >= 495:
-                if 140 <= event.pos[0] <= 280:
-                    BOARD3.get_click(event.pos, 1, side, 0)
+                if 140 <= event.pos[0] <= 280 and 495 <= event.pos[1] <= 530:
+                    BOARD3.get_click(event.pos, 1, side, 0, pos)
                     kol = BOARD3.get_cell(event.pos)[0] + 1
                     side = 'right'
-                elif 460 <= event.pos[0] <= 600:
-                    BOARD4.get_click(event.pos, 1, side, 0)
+                    pos = BOARD3.get_cell(event.pos)
+                elif 460 <= event.pos[0] <= 600 and 495 <= event.pos[1] <= 530:
+                    BOARD4.get_click(event.pos, 1, side, 0, pos)
                     kol = BOARD4.get_cell(event.pos)[0] + 1
                     side = 'down'
+                    pos = BOARD4.get_cell(event.pos)
                 n = 1
             global P4_COUNT, P3_COUNT, P2_COUNT, P1_COUNT, RED_FLAG_POLE
             if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
-                if P4_COUNT == 0 and P3_COUNT == 0 and P2_COUNT == 0 and P1_COUNT == 0 and w_kol == 0:  # функция, чтоб понять, что W нажата,
-                    # т.е. для переключения на второе поле
-                    P4_COUNT, P3_COUNT, P2_COUNT, P1_COUNT = 1, 2, 3, 4  # возрождаю первоначальные значения для нового поля
+                if P4_COUNT == 0 and P3_COUNT == 0 and P2_COUNT == 0 and P1_COUNT == 0 and w_kol == 0:
+                    P4_COUNT, P3_COUNT, P2_COUNT, P1_COUNT = 1, 2, 3, 4
                     hide_pole(1)
-                    W_PRESS_COUNT = 1  # переменная, которая будет указателем для второго поля, что ему можно активироваться
-                    RED_FLAG_POLE = True  # флаг, благодаря которому после передачи в него значения True
-                    # первое поле изменять будет нельзя
+                    W_PRESS_COUNT = 1
+                    RED_FLAG_POLE = True
                     w_kol = 1
+                elif P4_COUNT == 0 and P3_COUNT == 0 and P2_COUNT == 0 and P1_COUNT == 0 and w_kol == 1:
+                    del_error_f()
+                    hide_pole(2)
+                    game_window()
                 else:
                     error_before_w()
             if event.type == pygame.MOUSEBUTTONDOWN and event.pos[1] <= 470 and n == 1:
                 if event.pos[0] <= 430 and not RED_FLAG_POLE:
-                    BOARD1.get_click(event.pos, kol, side, 1)  # единички и двойки тут как раз-таки для определения
-                    # в функции отрисовки полей (левое или правое участвует в процессе)
+                    BOARD1.get_click(event.pos, kol, side, 1, pos)
                 elif event.pos[0] >= 431 and W_PRESS_COUNT == 1:
-                    BOARD2.get_click(event.pos, kol, side, 2)
+                    BOARD2.get_click(event.pos, kol, side, 2, pos)
                 else:
                     error_wrong_pole()
                 kol = 1
@@ -417,8 +448,46 @@ def main_screen():
         pygame.display.flip()
 
 
-def main_screen2():
-    pass
+def game_window():
+    screen.fill((0, 0, 0))
+    global BOARD1, BOARD2, BOARD3, BOARD4, W_PRESS_COUNT
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 46)
+    letters = 'абвгдежзик'
+    letters_x_1 = 12
+    letters_x_2 = 408
+    letters_y = 88
+    for elem in letters:
+        text = font.render(elem, True, (255, 255, 255))
+        screen.blit(text, (letters_x_1, letters_y))
+        screen.blit(text, (letters_x_2, letters_y))
+        letters_y += 35
+    text = font.render("1  2  3  4  5  6  7  8  9 10", True, (255, 255, 255))
+    screen.blit(text, (55, 55))
+    screen.blit(text, (450, 55))
+    font_names = pygame.font.SysFont('arial', 45)
+    name_1 = font_names.render("PLAYER 1", True, (0, 255, 255))
+    name_2 = font_names.render("PLAYER 2", True, (0, 255, 255))
+    screen.blit(name_1, (55, 5))
+    screen.blit(name_2, (450, 5))
+
+    hod = font_names.render("Ходит:", True, (255, 255, 255))
+    screen.blit(hod, (55, 500))
+
+    n = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(1)
+                player(n)
+                n += 1
+        BOARD1.render(screen)
+        BOARD2.render(screen)
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
@@ -434,8 +503,6 @@ if __name__ == '__main__':
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) and SPACE_PRESS_COUNT == 0:
                 SPACE_PRESS_COUNT += 1
                 main_screen()
-            if (event.type == pygame.KEYDOWN and event.key == pygame.K_w) and W_PRESS_COUNT == 2:
-                main_screen2()
         pygame.display.flip()
 
 pygame.quit()
