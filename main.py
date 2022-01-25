9204f09e1795b4e3d9c3118cc80efdb0a2a61fd5
yanak <yanakasaeva2006@gmail.com>
2022-01-25 19:31:38 +0300

    just commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+import random
 
 SPACE_PRESS_COUNT = 0
 W_PRESS_COUNT = 0
@@ -14,22 +15,51 @@ P4_COUNT = 1
 P3_COUNT = 2
 P2_COUNT = 3
 P1_COUNT = 4
+ALL_SPRITE = pygame.sprite.Group()
 
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
+    '''if not os.path.isfile(fullname):
         print(f'Файл с изображением {fullname} не существует')
-        sys.exit()
+        sys.exit()'''
     image = pygame.image.load(fullname)
-    if color_key is not None:
+    '''if color_key is not None:
         image = image.convert()
         if color_key == -1:
             color_key = image.get_at((0, 0))
         image.set_colorkey(color_key)
     else:
-        image = image.convert_alpha()
+        image = image.convert_alpha()'''
     return image
+
+
+class Particle(pygame.sprite.Sprite):
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(ALL_SPRITE)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+        self.gravity = 10
+
+    def update(self):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect(screen):
+            self.kill()
+
+
+def create_particles(position):
+    particle_count = 20
+    numbers = range(-5, 6)
+    for i in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
 
 
 class Board():
@@ -155,8 +185,6 @@ class Board():
                 error_place()  # если ошибка есть то вывожу оповещение
 
             RED_FLAG = False
-            for elem in matrix_1:
-                print(elem)
 
         elif side == 'down':
             UNCLICK = True
@@ -208,8 +236,6 @@ class Board():
                 error_place()
 
             RED_FLAG = False
-            for elem in matrix_1:
-                print(elem)
 
     def get_click(self, mouse_pos, n, side, pole, pos):
         cell = self.get_cell(mouse_pos)
@@ -217,17 +243,22 @@ class Board():
             self.on_click(cell, n, side,
                           pole, pos)
 
-    def fire(self, cell, side):
-        if side == 1:
+    def fire(self, cell, pole):
+        if pole == 1:
             self.matrix = matrix_1
         else:
             self.matrix = matrix_2
         if self.matrix[cell[1]][cell[0]] == 1:
             print('попал')
+            create_particles(get_coords(cell, pole))
+            draw_point(cell, pole)
         else:
             print('не попал')
         # тут надо по прототипу функции ok_click найти двойки рядом и вывести,
         # можешь этого не делать, я после репета вставлю
+        # сделать проверку на то, что туда уже стреляли(3 в матрицу занести и проверять)
+        # сделать чтобы попавший игрок ходил снова
+        # еще нажатия не работают: если два раза нажать на кораблик а потом на поле, то он все равно поставится
 
     def coor_fire(self, mouse_pos, side):
         cell = self.get_cell(mouse_pos)
@@ -236,7 +267,6 @@ class Board():
         print(mouse_pos)
 
 
-# класс для того чтобы в матрицу не заносились данные корабликов для выбора
 class Board_Choose_Ship(Board):
 
     def on_click(self, cell_coords, n, side, board, pos):
@@ -246,6 +276,19 @@ class Board_Choose_Ship(Board):
             UNCLICK = False
         else:
             self.board[cell_coords[1]][cell_coords[0]] = 1 - self.board[cell_coords[1]][cell_coords[0]]
+
+
+def get_coords(cell, pole):
+    if pole == 1:
+        x = cell[0] * 35 + 45 - 35 // 2
+    else:
+        x = cell[0] * 35 + 440 - 35 // 2
+    y = cell[1] * 35 + 85 - 35 // 2
+    return x, y
+
+
+def draw_point(cell, pole):
+    pygame.draw.circle(screen, (0, 255, 255), get_coords(cell, pole), 2)
 
 
 def del_error_f():
@@ -348,8 +391,7 @@ def start_screen():
         y += 50
 
 
-def main_screen():
-    global BOARD1, BOARD2, BOARD3, BOARD4, W_PRESS_COUNT
+def boards():
     screen.fill((0, 0, 0))
     font = pygame.font.Font(None, 46)
     letters = 'абвгдежзик'
@@ -370,40 +412,44 @@ def main_screen():
     screen.blit(name_1, (55, 5))
     screen.blit(name_2, (450, 5))
 
-    all_sprite = pygame.sprite.Group()
+
+def main_screen():
+    global BOARD1, BOARD2, BOARD3, BOARD4, W_PRESS_COUNT, ALL_SPRITE
+    boards()
+
     ship1 = pygame.sprite.Sprite()
     ship1.image = load_image('small_gor.jpg')
     ship1.rect = ship1.image.get_rect()
-    all_sprite.add(ship1)
+    ALL_SPRITE.add(ship1)
     ship2 = pygame.sprite.Sprite()
     ship2.image = load_image('mid_gor.jpg')
     ship2.rect = ship2.image.get_rect()
-    all_sprite.add(ship2)
+    ALL_SPRITE.add(ship2)
     ship3 = pygame.sprite.Sprite()
     ship3.image = load_image('mid_max_gor.jpg')
     ship3.rect = ship3.image.get_rect()
-    all_sprite.add(ship3)
+    ALL_SPRITE.add(ship3)
     ship4 = pygame.sprite.Sprite()
     ship4.image = load_image('max_gor.jpg')
     ship4.rect = ship4.image.get_rect()
-    all_sprite.add(ship4)
+    ALL_SPRITE.add(ship4)
 
     ship1vert = pygame.sprite.Sprite()
     ship1vert.image = load_image('small_vert.jpg')
     ship1vert.rect = ship1vert.image.get_rect()
-    all_sprite.add(ship1vert)
+    ALL_SPRITE.add(ship1vert)
     ship2vert = pygame.sprite.Sprite()
     ship2vert.image = load_image('mid_vert.jpg')
     ship2vert.rect = ship2vert.image.get_rect()
-    all_sprite.add(ship2vert)
+    ALL_SPRITE.add(ship2vert)
     ship3vert = pygame.sprite.Sprite()
     ship3vert.image = load_image('mid_max_vert.jpg')
     ship3vert.rect = ship3vert.image.get_rect()
-    all_sprite.add(ship3vert)
+    ALL_SPRITE.add(ship3vert)
     ship4vert = pygame.sprite.Sprite()
     ship4vert.image = load_image('max_vert.jpg')
     ship4vert.rect = ship4vert.image.get_rect()
-    all_sprite.add(ship4vert)
+    ALL_SPRITE.add(ship4vert)
 
     ship1.rect.x = 143
     ship1.rect.y = 497
@@ -455,6 +501,7 @@ def main_screen():
                 elif P4_COUNT == 0 and P3_COUNT == 0 and P2_COUNT == 0 and P1_COUNT == 0 and w_kol == 1:
                     del_error_f()
                     hide_pole(2)
+                    W_PRESS_COUNT = 2
                     game_window()
                 else:
                     error_before_w()
@@ -472,36 +519,19 @@ def main_screen():
         BOARD2.render(screen)
         BOARD3.render(screen)
         BOARD4.render(screen)
-        all_sprite.draw(screen)
+        ALL_SPRITE.draw(screen)
         pygame.display.flip()
 
 
 def game_window():
     screen.fill((0, 0, 0))
     global BOARD1, BOARD2, BOARD3, BOARD4, W_PRESS_COUNT
-    screen.fill((0, 0, 0))
-    font = pygame.font.Font(None, 46)
-    letters = 'абвгдежзик'
-    letters_x_1 = 12
-    letters_x_2 = 408
-    letters_y = 88
-    for elem in letters:
-        text = font.render(elem, True, (255, 255, 255))
-        screen.blit(text, (letters_x_1, letters_y))
-        screen.blit(text, (letters_x_2, letters_y))
-        letters_y += 35
-    text = font.render("1  2  3  4  5  6  7  8  9 10", True, (255, 255, 255))
-    screen.blit(text, (55, 55))
-    screen.blit(text, (450, 55))
+    boards()
     font_names = pygame.font.SysFont('arial', 45)
-    name_1 = font_names.render("PLAYER 1", True, (0, 255, 255))
-    name_2 = font_names.render("PLAYER 2", True, (0, 255, 255))
-    screen.blit(name_1, (55, 5))
-    screen.blit(name_2, (450, 5))
-
     hod = font_names.render("Ходит:", True, (255, 255, 255))
     screen.blit(hod, (55, 500))
 
+    winner = 'нет'
     n = 0
     player(n)
 
@@ -531,6 +561,26 @@ def game_window():
         pygame.display.flip()
 
 
+def end_window():
+    global WINNER
+    screen.fill((0, 0, 0))
+    font_1 = pygame.font.Font(None, 50)
+    font_2 = pygame.font.Font(None, 32)
+    results = ['Результаты игры',
+               f'Победил: {WINNER}', 'Осталось кораблей на поле: ?',
+               'Всего было сделано ходов: ?']
+    y = 15
+    for elem in results:
+        if elem == 'Результаты игры':
+            text = font_1.render(elem, True, (0, 255, 255))
+        else:
+            text = font_2.render(elem, True, (255, 255, 255))
+        text_x = width // 2 - text.get_width() // 2
+        text_y = y
+        screen.blit(text, (text_x, text_y))
+        y += 50
+
+
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 600
@@ -542,8 +592,11 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 terminate()
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) and SPACE_PRESS_COUNT == 0:
-                SPACE_PRESS_COUNT += 1
+                SPACE_PRESS_COUNT = 1
                 main_screen()
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE)\
+                    and SPACE_PRESS_COUNT == 1 and W_PRESS_COUNT == 2:
+                end_window()
         pygame.display.flip()
 
 pygame.quit()
